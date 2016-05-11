97583722e3ccedc80755f231a7d92980add0403e
Ryan Mathie <Ryan_Mathie@ryans-macbook-pro.local>
2016-05-11 00:24:52 -0400

    Initial Commit

---
+++ ArrivedThere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArrivedThere
+//
+//  Created by Ryan Mathie on 5/11/16.
+//  Copyright © 2016 Ape15 DEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
